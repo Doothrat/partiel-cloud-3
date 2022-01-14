32bdbd3f752d66f576fc4bd88d8f943d8a734476
Doothrat <60654806+Doothrat@users.noreply.github.com>
2022-01-14 10:01:13 +0100

    Add files via upload

---
+++ exo-image-gallery/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  s3: {
+    accessKeyId: 'OE6PFKO64084CNK2Y0CF', // TODO
+    secretAccessKey: 'eHWuFaTfGBrp6prGLFDq4qCOfCDc67HQYDSADCQV', // TODO
+    endpoint: 's3.eu-west-1.wasabisys.com', // TODO
+    bucket: 'esgi-cloud-exam-bucket-6c9db1', // TODO
+  },
+  suffix: {
+    small: '_small.jpg',
+    full: '_full.jpg'
+  }
+};
